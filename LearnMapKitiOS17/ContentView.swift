fcd01f61b5bceff42bb8a468ae76b339d8584daf
TomHa <tomhackbarth@gmail.com>
2023-06-19 12:20:34 -0400

    Initial Commit

---
+++ LearnMapKitiOS17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnMapKitiOS17
+//
+//  Created by tom hackbarth on 6/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnMapKitiOS17/LearnMapKitiOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMapKitiOS17App.swift
+//  LearnMapKitiOS17
+//
+//  Created by tom hackbarth on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnMapKitiOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
