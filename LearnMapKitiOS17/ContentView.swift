0a1c38d939be6b3a428c9d72ec2affe4812f61d0
TomHa <tomhackbarth@gmail.com>
2023-06-19 13:22:21 -0400

    puts a point on Denver

--- LearnMapKitiOS17/ContentView.swift
+++ LearnMapKitiOS17/ContentView.swift
@@ -6,19 +6,46 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State var placeAPin = false
+    @State var pinLocation :CLLocationCoordinate2D? = nil
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack{
+                Map{
+                    if let pl = pinLocation {
+                        Marker("here", coordinate: pl)
+                    }
+                }
+                .onTapGesture(perform: { pt in
+                    if (placeAPin){
+                        // Need to get the location coord from map
+                        
+                        pinLocation = CLLocationCoordinate2D(latitude: 39.742043, longitude: -104.991531)
+                        
+                    }
+                    placeAPin = false
+                })
+                .mapStyle(.standard(elevation: .realistic))
+                
+            }
+            .navigationTitle("Map View")
+            .toolbar {
+                if !placeAPin{
+                    Button("Place a Pin") {
+                        placeAPin = true
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
